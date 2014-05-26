11a055db6820f675a8e3bcf35bb6ede5a3b98de6
yan <a0726h77@gmail.com>
2014-05-26 14:45:38 +0800

    refactor mail library

--- Gmail.py
+++ Gmail.py
@@ -1,32 +1,47 @@
 # coding:utf-8
 
+import os
 import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.Header import Header
 from email.mime.text import MIMEText
+from email.MIMEBase import MIMEBase
+from email.Utils import formatdate
+from email import Encoders
 
-SERVER_HOST = "smtp.gmail.com"
-SERVER_PORT = 587
+HOST = "smtp.gmail.com"
+PORT = 587
 
 
 class Gmail():
     def __init__(self, username, password):
-        self.smtp = smtplib.SMTP(SERVER_HOST, SERVER_PORT)
+        self.smtp = smtplib.SMTP(HOST, PORT)
         self.smtp.starttls()
         self.smtp.login(username, password)
 
-    def send(self, sender, recipient, subject, text=None, html=None):
+    def send(self, sender, recipient, subject, text=None, html=None, files=None):
         msg = MIMEMultipart("alternative")
 
         msg['From'] = sender
         msg['To'] = recipient
+        msg['Date'] = formatdate(localtime=True)
         msg['Subject'] = Header(subject, "utf-8")
 
+        # 內文
         if text:
             msg.attach(MIMEText(text, "plain", "utf-8"))
         if html:
             msg.attach(MIMEText(html, "html", "utf-8"))
 
+        # 附加檔案
+        for f in files:
+            part = MIMEBase('application', "octet-stream")
+            part.set_payload(open(f, "rb").read())
+            Encoders.encode_base64(part)
+            part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(f))
+            msg.attach(part)
+
+        # 寄出
         self.smtp.sendmail(sender, recipient, msg.as_string())
 
     def __del__(self):
