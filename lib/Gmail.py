248e4173b2e04d40a4b096ef8f5ab5bd85e98c73
yan <a0726h77@gmail.com>
2014-06-09 14:23:01 +0800

    fix Gmail library

--- lib/Gmail.py
+++ lib/Gmail.py
@@ -14,36 +14,55 @@ PORT = 587
 
 
 class Gmail():
-    def __init__(self, username, password):
-        self.smtp = smtplib.SMTP(HOST, PORT)
+    def __init__(self):
+        self.smtp = smtplib.SMTP(HOST, PORT, timeout=5)
         self.smtp.starttls()
-        self.smtp.login(username, password)
 
-    def send(self, sender, recipient, subject, text=None, html=None, files=None):
-        msg = MIMEMultipart("alternative")
+    def login(self, username, password):
+        try:
+            self.smtp.login(username, password)
+            return True
+        except smtplib.SMTPAuthenticationError:
+            return False
 
-        msg['From'] = sender
-        msg['To'] = recipient
-        msg['Date'] = formatdate(localtime=True)
-        msg['Subject'] = Header(subject, "utf-8")
+    def send(self, sender, recipient, subject, text=None, html=None, files=[]):
+        try:
+            msg = MIMEMultipart("alternative")
 
-        # 內文
-        if text:
-            msg.attach(MIMEText(text, "plain", "utf-8"))
-        if html:
-            msg.attach(MIMEText(html, "html", "utf-8"))
+            msg['From'] = sender
+            msg['To'] = recipient
+            msg['Date'] = formatdate(localtime=True)
+            msg['Subject'] = Header(subject, "utf-8")
 
-        # 附加檔案
-        for f in files:
-            part = MIMEBase('application', "octet-stream")
-            part.set_payload(open(f, "rb").read())
-            Encoders.encode_base64(part)
-            part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(f))
-            msg.attach(part)
+            # 內文
+            if text:
+                msg.attach(MIMEText(text, "plain", "utf-8"))
+            if html:
+                msg.attach(MIMEText(html, "html", "utf-8"))
 
-        # 寄出
-        self.smtp.sendmail(sender, recipient, msg.as_string())
+            # 附加檔案
+            for f in files:
+                part = MIMEBase('application', "octet-stream")
+                part.set_payload(open(f, "rb").read())
+                Encoders.encode_base64(part)
+                part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(f))
+                msg.attach(part)
+
+            # 寄出
+            return self.smtp.sendmail(sender, recipient, msg.as_string())
+        except Exception, error:
+            print "Unable to send e-mail: '%s'." % str(error)
+
+            return False
+
+    def quit(self):
+        try:
+            # Should be self.smtp.quit(), but that crashes...
+            self.smtp.close()
+
+            return True
+        except:
+            return False
 
     def __del__(self):
-        # Should be self.smtp.quit(), but that crashes...
-        self.smtp.close()
+        self.quit()
