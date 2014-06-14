72aef467369ee51df95e974e46d830677c9463d7
yan <a0726h77@gmail.com>
2014-06-15 00:17:12 +0800

    refactor post PTT article

--- h4_invitation_notifier_ptt.py
+++ h4_invitation_notifier_ptt.py
@@ -1,19 +1,33 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# encoding: utf-8
 
 import common
-from Config import Config
+from Logger import Logger
+from lib.Config import Config
 from lib.PTT import PTT
 
-ID = Config()['bbs']['user']
-PASSWORD = Config()['bbs']['pass']
+logger = Logger('h4_invitation_notifier_ptt').__new__()
+
+config = Config()
+ID = config['bbs']['user']
+PASSWORD = config['bbs']['pass']
 PartyDate = common.thisThursday()
 
 board = 'Linux'
-subject = 'HackingThursday 固定聚會 (' + PartyDate + ')'
+subject = 'HackingThursday 固定聚會 (%s)' % PartyDate
 content = common.html2txt(common.get_wikidot_content_body('http://www.hackingthursday.org/invite'))
 
 if __name__ == '__main__':
-    ptt = PTT(ID, PASSWORD)
-    ptt.enter(board)
-    ptt.post(subject, content)
-    ptt.quit()
+    ptt = PTT()
+
+    if ptt.login(ID, PASSWORD):
+        logger.info('login ptt')
+
+    if ptt.enter(board):
+        logger.info('enter %s board' % board)
+
+    if ptt.post(subject, content):
+        logger.info('post article')
+
+    if ptt.quit():
+        logger.info('quit ptt')
