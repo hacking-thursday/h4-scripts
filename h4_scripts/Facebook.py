f9ce63324b822f8d9e929c0304bf29afff7de8ad
yan <a0726h77@gmail.com>
2015-08-09 12:14:06 +0800

    fix login error

--- h4_scripts/Facebook.py
+++ h4_scripts/Facebook.py
@@ -33,12 +33,32 @@ class Facebook():
 
         self.opener = opener
 
-        usock = self.opener.open('http://www.facebook.com')
+        # usock = self.opener.open('http://www.facebook.com')
+        usock = self.opener.open('https://m.facebook.com/login.php')
 
     def login(self, email, password):
-        url = 'https://www.facebook.com/login.php?login_attempt=1'
+        # url = 'https://www.facebook.com/login.php?login_attempt=1'
+        url = 'https://m.facebook.com/login.php'
 
-        data = "locale=en_US&lsd=%s&lgnrnd=215019_4mpx&lgnjs=1400475025&email=%s&pass=%s" % ('AVoy-05c', email, password)
+        # data = "locale=en_US&lsd=%s&lgnrnd=215019_4mpx&lgnjs=1400475025&email=%s&pass=%s" % ('AVoy-05c', email, password)
+        data = urllib.urlencode({
+            'lsd': 'AVrDL7ho',
+            'display': '',
+            'enable_profile_selector': '',
+            'legacy_return': '1',
+            'profile_selector_ids': '',
+            'trynum': '1',
+            'timezone': '',
+            'lgndim': '',
+            'lgnrnd': '175413_BJ5y',
+            'lgnjs': 'n',
+            'email': email,
+            'pass': password,
+            'default_persistent': '0',
+            'login': 'Log+In',
+            'li': 'v7PGVRnZ8eCBpPdGw4k-3B3D',
+            'm_ts': '',
+            })
 
         usock = self.opener.open(url, data)
         line = usock.read()
